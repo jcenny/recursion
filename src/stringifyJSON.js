71d418aac41c4537324f5052f1abeb0c689472f2
jcenny <jcenny26@gmail.com>
2019-10-15 16:58:03 -0700

    Create JSON.stringify from scratch using recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,50 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+  var result = '';
+  var type = typeof(obj);
+  // object as boolean/number/null
+  if (type === 'number' || type === 'boolean' || obj === null) {
+    result += obj;
+  }
+  // object as string
+  if (type === 'string') {
+    result = '"' + obj + '"';
+  }
+  // object as array
+  if (Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+    result += '[';
+    for (let i = 0; i < obj.length; i++) {
+      result += stringifyJSON(obj[i]);
+      if (i !== obj.length - 1) {
+        result += ',';
+      } else {
+        result += ']';
+      }
+    }
+  }
+// object as object
+  if (type === 'object' && Array.isArray(obj) === false && obj !== null) {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    }
+    for (let key in obj) {
+      if (typeof(obj[key]) === 'function' || obj[key] === undefined) {
+        return '{}';
+      }
+    }
+    result += '{';
+    for (let key in obj) {
+      result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+      if (key !== Object.keys(obj).pop()) {
+        result += ',';
+      } else {
+        result += '}';
+      }
+    }
+  }
+  return result;
+}
